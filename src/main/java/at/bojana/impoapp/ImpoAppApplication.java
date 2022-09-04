d77db4e74f80b3d9692c687942d8adfef4c26779
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-04 18:00:40 +0200

    Initial commit

---
+++ src/main/java/at/bojana/impoapp/ImpoAppApplication.java
@@ -0,0 +1,13 @@
+package at.bojana.impoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/bojana/impoapp/ImpoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bojana.impoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
